4bda665610711be71d919f3742466fc0b2d3481e
Jan Nicklas <j.nicklas@flowconcept.de>
2013-10-14 23:56:01 +0200

    Generate source map and add google compiler

---
+++ closure_compiler/externs/define.js
@@ -0,0 +1,13 @@
+/**
+ * @fileoverview Externs for requireJS
+ * @externs
+ */
+
+/**
+ * @param {Array.<string>} arg1
+ * @param {function(?): ?} arg2
+ */
+function define(arg1, arg2) {}
+
+/** @type {boolean} */
+define.amd;
