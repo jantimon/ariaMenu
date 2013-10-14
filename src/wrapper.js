427e42856552801a58961c6800ae42b51da7e022
Jan Nicklas <j.nicklas@flowconcept.de>
2013-10-14 23:54:31 +0200

    Separate AMD code and none AMD code

---
+++ src/wrapper.js
@@ -0,0 +1,20 @@
+/** Copyright (c) 2013 Jan Nicklas Released under MIT license */
+
+/* global define: false, jQuery: true */
+/* jshint sub:true */
+
+// RequireJS amd factory
+// http://stackoverflow.com/questions/10918063/how-to-make-a-jquery-plugin-loadable-with-requirejs#answer-11890239
+(function (factory) {
+  'use strict';
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['jquery'], factory);
+  } else {
+    // Run without AMD
+    factory(jQuery);
+  }
+}(function ($) {
+  'use strict';
+  // @@ code @@ //
+}));
