22acd145a601d6047ac94b383ebf069bc27d23e6
Jan Nicklas <j.nicklas@me.com>
2013-12-02 15:31:13 +0100

    Update SauceLabs key

--- runsauce.sh
+++ runsauce.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-SAUCE_USERNAME=jantimon SAUCE_ACCESS_KEY=ef3d2b4e-b257-40df-a4af-533a5aa2e584 node node_modules/intern/runner.js config=tests/sauceLabs/intern.config.js
+SAUCE_USERNAME=ariaMenu SAUCE_ACCESS_KEY=ecd461a4-d549-409e-a7da-40533e8fba93 node node_modules/intern/runner.js config=tests/sauceLabs/intern.config.js
 genhtml lcov.info --output-directory docs/lcov
